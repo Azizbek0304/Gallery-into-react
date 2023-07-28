36d81a4ee2bd4e4d644d420931bd54273840dc4c
Azizbek0304 <115786719+Azizbek0304@users.noreply.github.com>
2023-07-28 22:16:07 +0500

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import Header from './components/PageWrapper';
 import './style.css';
 
 export default function App() {
@@ -7,6 +8,7 @@ export default function App() {
     <div>
       <div id="page-stripes" style={{ display: 'none' }}></div>
       <Header />
+      {/* <PageWrapper /> */}
     </div>
   );
 }
